de86518201c557ac1eee9106591db0db07b99944
gyougi.rin@rnai.co.jp <gyougi.rin@rnai.co.jp>
2017-12-15 17:11:23 +0900

    Bug fixed

--- src/Linxy49/LaraZaif/Client.php
+++ src/Linxy49/LaraZaif/Client.php
@@ -30,4 +30,44 @@ class Client
         $data['bids'] = $depth['bids'];
         return $data;
     }
+
+    private function _format(array $data)
+    {
+        $asks = $this->_lastest($this->_sort($data['asks']), 10, false);
+        $bids = $this->_lastest($this->_sort($data['bids']), 10, true);
+        $result['price'] = $data['price'];
+        $result['asks'] = $asks;
+        $result['bids'] = $bids;
+        return $result;
+    }
+
+    /**
+     *　ソート
+     */
+    private function _sort($array)
+    {
+        uasort($array, function ($a, $b) {
+            if($a[0] == $b[0]) {
+                return 0;
+            }
+            return ($a[0] > $b[0]) ? -1 : 1;
+        });
+        return $array;
+    }
+
+    /**
+     * 最新の情報を取得する
+     * @param $array
+     * @param $length 件数
+     * @param $type true:先頭 | false:末尾
+     */
+    private function _lastest($array, $length, $type)
+    {
+        if ($type) {
+            return array_slice($array, 0, $length);
+        } else {
+            $count = count($array) - $length;
+            return array_slice($array, $count, $length);
+        }
+    }
 }
